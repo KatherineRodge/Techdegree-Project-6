21900b243d2224fee435ac7e0990dafe85c8027a
KatherineRodge <rodgek@mail.gvsu.edu>
2019-06-16 15:04:43 -0400

    Update
    Added images, data, gitignore

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+//const { data } = require('data.json');
+
+app.set('view engine', 'pug');
+app.use('/static', express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/project', (req, res) => {
+  res.render('project');
+});
+
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000')
+});
