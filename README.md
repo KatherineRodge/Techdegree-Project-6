e4d361c0c90db6ab1d988314e9676ddd73627517
KatherineRodge <rodgek@mail.gvsu.edu>
2019-06-22 12:36:55 -0400

    Final Updates

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Techdegree-Project-6
+
+Portfolio project rendered using PUG templates 
+
+CSS Changes:
+H1 font-family changed and color
+button background color changed
+button:hover background color changed
+box shadow added to portfolio-index thumbnail
--- app.js
+++ app.js
@@ -6,18 +6,23 @@ const { projects } = require('./data.json');
 app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
 
+
+//Sets ups layout
 app.get('/layout', (req, res) => {
   res.render('layout');
 });
 
+//renders index and project data from .json file
 app.get('/', (req, res) => {
   res.render('index', {projects: projects});
 });
 
+//renders about page
 app.get('/about', (req, res) => {
   res.render('about');
 });
 
+//renders each project data when clicked on thumbnail
 app.get( '/project/:id', ( req, res ) => {
   const projectId = req.params.id;
   const currentProject = projects[projectId];
@@ -35,6 +40,18 @@ app.get( '/project/:id', ( req, res ) => {
   res.render('project', currentProjectData);
 });
 
+//error coding (via team treehouse express basics: middleware course)
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error');
+});
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000')
